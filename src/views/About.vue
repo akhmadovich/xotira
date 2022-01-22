7055d816c5ee4ff77550653f35ccf7d729366dbe
JAKHONGIR SHAPOATOV <islomjahongir@gmail.com>
2022-01-22 09:48:32 +0500

    changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page23</h1>
+    <h1>This is an about page</h1>
+    <p><span class="bi bi-app-indicator fs-1"></span></p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p>Lorem ipsum dolor sit amet.</p>
   </div>
 </template>
 
