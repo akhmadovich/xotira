fb6b3aa5baa39ee2ffed2f81ba6ef22d6bb69471
JAKHONGIR SHAPOATOV <islomjahongir@gmail.com>
2022-01-22 10:34:27 +0500

    changes

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,5 +2,7 @@
   <div class="about">
     <h1>This is an about page</h1>
     <p><span class="bi bi-arrow-up-left-square-fill fs-1"> || <span class="bi bi-apple fs-1"></span></span></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi nisi mollitia quaerat explicabo recusandae sunt in non magnam, maiores consectetur nesciunt, facere vero sint reprehenderit ab asperiores. Sunt, inventore molestiae.</p>
+    
   </div>
 </template>
