69e6f106fcabd2edcf1c9195c70cc776aa3bdd3d
JAKHONGIR SHAPOATOV <islomjahongir@gmail.com>
2022-01-24 09:59:31 +0500

    saved

--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="cart">
         <h1>Cart page</h1>
+        <p><span class="bi bi-arrow-up-left-square-fill fs-1"> || <span class="bi bi-apple fs-1"></span></span></p>
     </div>
 </template>
